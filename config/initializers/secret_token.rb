81d5168e8c30b07d10a5f2d6f0295d92a38a28ce
Michael O'Keefe <okeefm@rpi.edu>
2013-12-24 13:28:19 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SensorcachingRails::Application.config.secret_key_base = 'bfca5faa963e40add057450ca1af9fbee0372f6880096146d78f73d1f86ed3fb6600587c91f70e559f22b65d36f1d585f4704ded77b392d1f853bcf240c81a23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SensorcachingRails::Application.config.session_store :cookie_store, key: '_sensorcaching_rails_session'
