7195570592162c4071c68fa621957efa3384b029
Michael O'Keefe <okeefm@rpi.edu>
2013-12-31 11:06:15 -0500

    Moved marker generation into a partial

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,18 +1,8 @@
 class VisitorsController < ApplicationController
-
+  helper SensorsHelper
+  
   def new
     @sensors = Sensor.all
-    @hash = Gmaps4rails.build_markers(@sensors) do |sensor, marker|
-      marker.lat sensor.latitude
-      marker.lng sensor.longitude
-      marker.infowindow "<div>
-                          <h2>" + sensor.name + "</h2>
-                          <p>" + sensor.description + "</p>
-                          <p><strong>Nearest Address:</strong>" + sensor.address + "</p>
-                          <p><a href='/sensors/"+ sensor.id.to_s + "/'>More info</a></p>
-                          
-                        </div>"
-    end
   end
 
 end
--- app/helpers/sensors_helper.rb
+++ app/helpers/sensors_helper.rb
@@ -1,2 +1,16 @@
 module SensorsHelper
+  def markers_from_sensors(sensors)
+    hash = Gmaps4rails.build_markers(sensors) do |sensor, marker|
+      marker.lat sensor.latitude
+      marker.lng sensor.longitude
+      marker.infowindow "<div>
+                          <h2>" + sensor.name + "</h2>
+                          <p>" + sensor.description + "</p>
+                          <p><strong>Nearest Address:</strong>" + sensor.address + "</p>
+                          <p><a href='/sensors/"+ sensor.id.to_s + "/'>More info</a></p>
+                          
+                        </div>"
+    end
+    return hash
+  end
 end
