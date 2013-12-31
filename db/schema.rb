25ba6c8c59d5079c9e153b25faea426f172258f8
Michael O'Keefe <okeefm@rpi.edu>
2013-12-30 22:24:50 -0500

    Updated users model to add role bitmask

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131226041115) do
+ActiveRecord::Schema.define(version: 20131229190140) do
 
   create_table "sensor_data", force: true do |t|
     t.integer  "sensor_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20131226041115) do
     t.string   "zip"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "roles_mask"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
